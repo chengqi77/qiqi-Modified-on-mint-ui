83b31fcca731be20c2fd169c7c9b358bcb2d1be6
chengqi <2113265828@qq.com>
2018-08-27 16:16:34 +0800

    new1

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="hello">
+   
+    <mt-button size="large"  type="primary">66666</mt-button>
+  </div>
+</template>
+
+<script>
+import Vue from 'vue';
+import MtButton from './button.vue'
+//Vue.component(Button.name, Button);
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  },
+  components:{
+    MtButton,
+  }
+  
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
---
+++ src/components/button.vue
@@ -0,0 +1,94 @@
+<template>
+    <button :disabled="disabled"  :type="nativeType" class="mint-button"
+    :class="['mint-button--' + type, 'mint-button--' + size, {
+      'is-disabled': disabled,
+      'is-plain': plain
+    }]">
+        <label class="mint-button-text"><slot></slot></label>
+    </button>
+</template>
+<style lang="less">
+
+.mint-button--primary{
+   color: #fff;
+    background-color: #26a2ff;
+}
+.mint-button--default {
+    color: #656b79;
+    background-color: #f6f8fa;
+    box-shadow: 0 0 1px #b8bbbf;
+}
+.mint-button--normal, .mint-button--small {
+    display: inline-block;
+    padding: 0 12px;
+}
+.mint-button--danger {
+    color: #fff;
+    background-color: #ef4f4f;
+  padding: 5px;
+}
+.mint-button--large{
+    width: 100%;
+     font-size: 14px;
+    height: 50px;
+}
+.mint-button--small {
+    font-size: 14px;
+    height: 33px;
+}
+
+.mint-button-text{
+    color: white;
+}
+.mint-button {
+    -webkit-appearance: none;
+    -moz-appearance: none;
+    appearance: none;
+    border-radius: 4px;
+    border: 0;
+    box-sizing: border-box;
+    color: inherit;
+    display: block;
+    font-size: 18px;
+    height: 41px;
+    outline: 0;
+    overflow: hidden;
+    position: relative;
+    text-align: center;
+}
+</style>
+
+<script>
+export default {
+     name: 'mt-button',
+    
+    props: {
+    disabled: Boolean,
+    nativeType: String,
+    plain: Boolean,
+    type: {
+      type: String,
+      default: 'default',
+      validator(value) {
+        return [
+          'default',
+          'danger',
+          'primary'
+        ].indexOf(value) > -1;
+      }
+    },
+    size: {
+      type: String,
+      default: 'normal',
+      validator(value) {
+        return [
+          'small',
+          'normal',
+          'large'
+        ].indexOf(value) > -1;
+      }
+    }
+  }
+}
+</script>
+
